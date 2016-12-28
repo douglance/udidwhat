e0028e04a685734a74b62d8f826ace4082744de7
douglance <doug.lance@gmail.com>
2016-12-28 17:17:54 -0500

    Fourth try is the lucky charm.

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,18 @@
-print("Hello, world. UDIDWHAT!")
+import Kitura
+import HeliumLogger
+import Foundation
+
+HeliumLogger.use()
+
+let router = Router()
+
+router.get("/") {
+	request, response, next in
+	response.send("UDID WHAT!")
+	next()
+}
+
+let port = Int(ProcessInfo.processInfo.environment["PORT"] ?? "8090") ?? 8090
+
+Kitura.addHTTPServer(onPort: port, with: router)
+Kitura.run()
