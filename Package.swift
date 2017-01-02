a2a20a4d217e9197609c194d3516ae752f3792eb
douglance <doug.lance@gmail.com>
2017-01-02 15:51:49 -0500

    Added Stencil

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ let package = Package(
     dependencies: [
 		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
 		.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
-		.Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1)
+		.Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1),
+		.Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1)
+
 	]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,18 +1,52 @@
+import Foundation
 import Kitura
 import HeliumLogger
-import Foundation
+import LoggerAPI
+import KituraStencil
 
 HeliumLogger.use()
 
 let router = Router()
+router.setDefault(templateEngine: StencilTemplateEngine())
+
+router.all("/static", middleware: StaticFileServer())
 
 router.get("/") {
 	request, response, next in
-	response.send("UDID WHAT!")
-	next()
+	defer { next() }
+	try response.render("home", context: [:])
 }
 
-let port = Int(ProcessInfo.processInfo.environment["PORT"] ?? "8090") ?? 8090
+router.get("/staff/:name") {
+	request, response, next in
+	defer { next() }
+	
+	guard let name = request.parameters["name"] else { return }
+	
+	let bios = [
+		"kirk": "My name is James Kirk and I love snakes.",
+		"picard": "My name is Jean-Luc Picard and I'm mad for cats.",
+		"sisko": "My name is Benjamin Sisko and I'm all about the budgies.",
+		"janeway": "My name is Kathryn Janeway and I want to hug every hamster.",
+		"archer": "My name is Jonathan Archer and beagles are my thing."
+	]
+	
+	var context = [String: Any]()
+	context["people"] = bios.keys.sorted()
+	
+	if let bio = bios[name] {
+		context["name"] = name
+		context["bio"] = bio
+	}
+	
+	try response.render("staff", context: context)
+}
 
-Kitura.addHTTPServer(onPort: port, with: router)
+router.get("/contact") {
+	request, response, next in
+	defer { next() }
+	try response.render("contact", context: [:])
+}
+
+Kitura.addHTTPServer(onPort: 8090, with: router)
 Kitura.run()
