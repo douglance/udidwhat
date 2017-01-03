d7b60e17af5c6d91389ba00c5b4c53c50946b87c
douglance <doug.lance@gmail.com>
2017-01-03 11:00:35 -0500

    Testing continues

--- Sources/main.swift
+++ Sources/main.swift
@@ -17,29 +17,11 @@ router.get("/") {
 	try response.render("home", context: [:])
 }
 
-router.get("/staff/:name") {
+router.get("/static/enroll.mobileconfig") {
 	request, response, next in
 	defer { next() }
+	try response.send(download: "/static/enroll.mobileconfig")
 	
-	guard let name = request.parameters["name"] else { return }
-	
-	let bios = [
-		"kirk": "My name is James Kirk and I love snakes.",
-		"picard": "My name is Jean-Luc Picard and I'm mad for cats.",
-		"sisko": "My name is Benjamin Sisko and I'm all about the budgies.",
-		"janeway": "My name is Kathryn Janeway and I want to hug every hamster.",
-		"archer": "My name is Jonathan Archer and beagles are my thing."
-	]
-	
-	var context = [String: Any]()
-	context["people"] = bios.keys.sorted()
-	
-	if let bio = bios[name] {
-		context["name"] = name
-		context["bio"] = bio
-	}
-	
-	try response.render("staff", context: context)
 }
 
 router.get("/contact") {
